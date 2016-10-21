b0127026a270bf64ad0b82d3211c3481de2b7aaf
Andrey Kartashov <akartashov@gilt.com>
2016-10-21 19:30:14 -0400

    Tweaked startup arguments, better support for JSON-formatted messages.

--- Main.hs
+++ Main.hs
@@ -13,22 +13,24 @@ import           Control.Monad.IO.Class
 import           Control.Monad.Trans.AWS
 import qualified Data.Aeson as A
 import qualified Data.ByteString.Lazy as L
+import qualified Data.ByteString.Lazy.Char8 as L8
 import           Data.Conduit
 import qualified Data.Conduit.List as CL
 import qualified Data.List as DL
 import qualified Data.List.NonEmpty as NEL
 import           Data.Maybe
 import qualified Data.Text as T
+import qualified Data.Text.Encoding as TE
 import qualified Data.Text.IO as TIO
 import           Data.Time.Clock
 import           Data.Time.Clock.POSIX
 import           Data.Time.Format
-import           Network.AWS.Auth(credFile)
+import           Network.AWS.Auth (credFile)
 import           Network.AWS.CloudWatchLogs
 import           Network.AWS.Data
 import           Numeric.Natural
 import           Options.Applicative as OA
-import           System.Environment(lookupEnv)
+import           System.Environment (lookupEnv)
 import           System.IO
 
 
@@ -41,7 +43,9 @@ data App =
       , appStartTime:: String
       , appEndTime:: Maybe String
       , appFollow:: Bool
+      , appFollowDelaySeconds:: Int
       , appVerbose:: Bool
+      , appIncludeEventMetadata:: Bool
       , appFilterPattern:: Maybe String
       } deriving (Eq, Show)
 
@@ -51,7 +55,7 @@ instance A.ToJSON FilteredLogEvent where
     (catMaybes
       [ (("IngestionTime" A..=) . formatTimestamp) <$> ( e ^. fleIngestionTime )
       , ("LogStreamName"  A..=)                    <$> ( e ^. fleLogStreamName )
-      , ("Message"        A..=)                    <$> ( e ^. fleMessage       )
+      , ("Message"        A..=)                    <$> ( messageJSONValue e    )
       , (("Timestamp"     A..=) . formatTimestamp) <$> ( e ^. fleTimestamp     )
       , ("EventId"        A..=)                    <$> ( e ^. fleEventId       )
       ])
@@ -97,11 +101,21 @@ parseApp defaultStartTime = App
      <*> switch
          ( long "follow"
         <> short 'f'
-        <> help "Keep looking for more results. N.B. this can skip over some events." )
+        <> help "Keep looking for more results. N.B. this can skip over some events" )
+     <*> option auto
+         ( long "follow-delay-seconds"
+        <> short 'd'
+        <> value 10
+        <> showDefault
+        <> help "How many seconds to delay output by in the 'follow' mode" )
      <*> switch
          ( long "verbose"
         <> short 'v'
         <> help "Run in a verbose mode, for debugging" )
+     <*> switch
+         ( long "include-event-metadata"
+        <> short 'm'
+        <> help "Include event metadata in the output" )
      <*> (optional $ OA.argument str
          ( metavar "CWL filter pattern"
         <> help "E.g. '\"some-pattern\"' http://docs.aws.amazon.com/AmazonCloudWatch/latest/DeveloperGuide/FilterAndPatternSyntax.html" ))
@@ -117,15 +131,13 @@ main = do
     opts p = OA.info (helper <*> p)
       ( fullDesc
      <> header "Tail-like utility for Cloud Watch Logs."
-     <> footer "E.g. aws-logs -g my-log-group '\"da240fb2-9b9d-4b80-bac2-f3452e937919\"'" )
+     <> footer "E.g. aws-logs -g my-log-group '\"da240fb2-9b9d-4b80-bac2-f3452e937919\"' OR aws-logs -g my-log-group '{$.level = \"ERROR\"}'" )
 
 
 tailAWSLogs:: App -> IO ()
 tailAWSLogs App{..} = do
-
-  if appFollow && (isJust appEndTime)
-  then error $ "-f (follow) conflicts with -E (end time)"
-  else return ()
+  when (appFollow && (isJust appEndTime)) $ error $ "-f (follow) conflicts with -E (end time)"
+  when (appFollowDelaySeconds < 5) $ error "Log follow delay must be >= 5 seconds"
 
   r <- case fromText (T.pack appRegion)
          of Left e -> error e
@@ -136,8 +148,12 @@ tailAWSLogs App{..} = do
                  else Info
 
   printResults <- case appOutputFormat
-                    of "json" -> return printLogEventJSON
-                       "text" -> return printLogEventText
+                    of "json" -> if appIncludeEventMetadata
+                                 then return printLogEventJSON
+                                 else return printLogMessageJSON
+                       "text" -> if appIncludeEventMetadata
+                                 then return printLogEventText
+                                 else return printLogMessageText
                        x      -> error $ "Unsupported output format " <> x
 
   lgr <- newLogger logLevel stdout
@@ -181,16 +197,14 @@ tailAWSLogs App{..} = do
                                  =$= CL.concatMap (view flersEvents)
                                  $$ CL.mapM_ (liftIO . printResults)
 
-         -- 5 to accumulate more events and 5 to let them sync up to CWL
-         delaySecs = 10
-
          -- stop a few sec before current time, to give CW Logs agent time to send logs to service
          nextEndTs = do currentTime <- liftIO $ getCurrentTime
-                        return $ (utcToNat currentTime) - (fromInteger . toInteger) (delaySecs * 1000 `div` 2)
+                        return $ (utcToNat currentTime) - (fromInteger . toInteger) (appFollowDelaySeconds * 1000)
 
-         followResultsSince ts = do liftIO $ CC.threadDelay $ delaySecs * 1000000
+         followResultsSince ts = do liftIO $ hPutStrLn stderr $ "Sleeping for " <> (show appFollowDelaySeconds) <> " seconds ..."
+                                    liftIO $ CC.threadDelay $ appFollowDelaySeconds * 1000000
                                     endTs <- nextEndTs
-                                    liftIO $ hPutStrLn stderr $ "Searching for more events [" <> formatTimestamp ts <> " .. " <> formatTimestamp endTs <> "]"
+                                    liftIO $ hPutStrLn stderr $ "Searching for more events " <> formatTimestamp ts <> " .. " <> formatTimestamp endTs
                                     _ <- processResults $ fle & (fleStartTime .~ Just ts)
                                                               . (fleEndTime .~ Just endTs)
 
@@ -209,16 +223,32 @@ printLogEventText e = do
                             ]
       message = e ^. fleMessage
 
-  putStrLn $ concat $ DL.intersperse " " msgHeader
-  forM_ message (TIO.putStr)
-  L.putStr "\n\n"
+  putStr $ "[" <> (concat $ DL.intersperse " " msgHeader) <> "] "
+  forM_ message (TIO.putStrLn)
+
+
+-- | Message only, no event metadata
+printLogMessageText:: FilteredLogEvent -> IO ()
+printLogMessageText e = forM_ (e ^. fleMessage) TIO.putStrLn
 
 
 -- | One JSON object per line
 printLogEventJSON:: FilteredLogEvent -> IO ()
-printLogEventJSON e = do
-  L.putStr $ A.encode e
-  L.putStr "\n"
+printLogEventJSON e = L8.putStrLn $ A.encode e
+
+
+-- | One JSON object per line, message only, no metadata
+printLogMessageJSON:: FilteredLogEvent -> IO ()
+printLogMessageJSON e = forM_ (messageJSONValue e) $ L8.putStrLn . A.encode
+
+
+-- | If Message is already JSON-formatted, unescape it
+messageJSONValue:: FilteredLogEvent -> Maybe A.Value
+messageJSONValue e =
+  (do m <- e ^. fleMessage
+      (A.decode . L.fromStrict . TE.encodeUtf8) m
+  ) <|> (A.String <$> e ^. fleMessage) -- kep it in the original form if not JSON
+
 
 utcToNat:: UTCTime -> Natural
 utcToNat t = (fromInteger . round) $ (toRational (utcTimeToPOSIXSeconds t)) * 1000
